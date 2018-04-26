60ce7fcfdd0168ae5bd3985aa36a9fca511730ed
Go Saito <go@paravirt.org>
2018-04-27 08:35:48 +0900

    bump 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SdNotifyEx.MixProject do
   def project do
     [
       app: :sd_notify_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
